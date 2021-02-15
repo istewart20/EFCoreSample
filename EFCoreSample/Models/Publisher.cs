098001e116d835447272e87f1c7840516c5aa904
Iain Stewart <iain.stewart.08@gmail.com>
2021-02-15 00:54:12 +0000

    Add Author + Publisher classes + DbSet

---
+++ EFCoreSample/Models/Publisher.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreSample.Models
+{
+    public class Publisher
+    {
+        [Key]
+        public int Publisher_Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+    }
+}
