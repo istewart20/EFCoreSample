b4a999eedc85eccdffccbdc3bd4bde6781446337
Iain Stewart <iain.stewart.08@gmail.com>
2021-02-15 21:36:15 +0000

    Create AddCategoryData migration

---
+++ EFCoreSample/Migrations/20210215212741_AddCategoryData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreSample.Migrations
+{
+    public partial class AddCategoryData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO dbo.Categories VALUES ('Cat1')");
+            migrationBuilder.Sql("INSERT INTO dbo.Categories VALUES ('Cat2')");
+            migrationBuilder.Sql("INSERT INTO dbo.Categories VALUES ('Cat3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM dbo.Categories WHERE Name = 'Cat1'");
+            migrationBuilder.Sql("DELETE FROM dbo.Categories WHERE Name = 'Cat2'");
+            migrationBuilder.Sql("DELETE FROM dbo.Categories WHERE Name = 'Cat3'");
+        }
+    }
+}
