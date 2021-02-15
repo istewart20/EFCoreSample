c0956c0cc86094cc4dc679cc2eca9ed0d06d2118
Iain Stewart <iain.stewart.08@gmail.com>
2021-02-15 11:28:38 +0000

    Create migration for BookDetail and Category tables

---
+++ EFCoreSample/Migrations/20210215112001_AddFKToBookDetailAndCategory.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreSample.Migrations
+{
+    public partial class AddFKToBookDetailAndCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookDetailId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "BookDetails",
+                columns: table => new
+                {
+                    BookDetail_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfChapters = table.Column<int>(type: "int", nullable: false),
+                    NumberOfPages = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookDetails", x => x.BookDetail_Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_BookDetailId",
+                table: "Books",
+                column: "BookDetailId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books",
+                column: "CategoryId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookDetails_BookDetailId",
+                table: "Books",
+                column: "BookDetailId",
+                principalTable: "BookDetails",
+                principalColumn: "BookDetail_Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Categories_CategoryId",
+                table: "Books",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookDetails_BookDetailId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Categories_CategoryId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "BookDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_BookDetailId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "BookDetailId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Books");
+        }
+    }
+}
